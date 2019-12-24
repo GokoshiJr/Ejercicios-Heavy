729a39065d080b7d77f0bcddb5315b30d470c25d
Gokoshi Jr <julioagonzalez18@gmail.com>
2019-12-23 22:40:08 -0400

    Version 2

---
+++ Hipica.py
@@ -0,0 +1,34 @@
+"""El club hípico esta organizando la competencia de salto anual del estado
+Carabobo, desarrolle un programa que ayude a determinar el tiempo final del competidor"""
+# Declaración de Variables
+jinete = caballo = ""
+minutos = segundos = tiempoRealizado = tiempoPermitido = 0
+obstaculos = penaTiempo = penaDerribo = ponderacionFalta = tiempoFinal = 0
+tiempoPermitido = 90
+# Entrada de datos
+print("-"*30)
+print("\t Datos")
+print("-"*30)
+jinete = input("Nombre del jinete: ")
+caballo = input("Nombre del caballo: ")
+minutos = int(input("Minutos del recorrido: "))
+segundos = int(input("Segundos del recorrido: "))
+obstaculos = int(input("Obstaculos derribados: "))
+print("-"*30)
+print("\t Resultados")
+print("-"*30)
+# Proceso
+tiempoRealizado = ((minutos * 60) + segundos)
+if obstaculos <= 10:
+    if tiempoRealizado > tiempoPermitido:
+        penaTiempo = tiempoRealizado - tiempoPermitido
+    if obstaculos > 0:
+        if obstaculos <= 5:
+            ponderacionFalta = 4
+        else:
+            ponderacionFalta = 6
+        penaDerribo = obstaculos * ponderacionFalta
+    tiempoFinal = tiempoRealizado + penaTiempo + penaDerribo
+    print(jinete,"montando a", caballo)
+    print("Realizó el recorrido en",tiempoFinal,"segundos")
+
--- Vuelos1.py
+++ Vuelos1.py
@@ -1,17 +1,17 @@
-# Ejercicio de los vuelos
+# Programa de Vuelos
 
 # Declaración de Variables
 numero = estatus = ""
 hora = minuto = horaSalida = minutoSalida = acum = cont = 0
 bandera = False
-
+# Abrimos el archivo .txt
 archivo = open("Vuelos.txt") 
 print("{0:4}""{1:>15}""{2:>12}""{3:>15}".format("Numero","Hora Prog","Demora","Estatus"))
+# Proceso
+for linea in archivo: # Con el for leemos cada linea de nuestro archivo .txt 
 
-for linea in archivo:
-
-    columna = linea.split(",")
-    numero = columna[0]
+    columna = linea.split(",") # Con el .split tomamos como columna lo que este separado por ","
+    numero = columna[0] # capturamos en una variable lo que este en la posición(columna) 0 y así sucesivamente
     hora = int(columna[1])
     minuto = int(columna[2])
     horaSalida = int(columna[3])
@@ -27,7 +27,7 @@ for linea in archivo:
         estatus = "Salió tarde"
         acum += demora
         cont += 1
-        if (bandera == False):
+        if (bandera == False): # switch para capturar por primera vez y comparar cual demora más
             bandera = True
             demoraTarde = demora
         elif (demora > demoraTarde):
@@ -41,17 +41,9 @@ for linea in archivo:
     
 horaTarde=(acum/60)/cont
 minutosTarde=(horaTarde*60)/cont
-
+# Salida de Datos
 print("")
 print(" Resultados")
 print(" Tiempo promedio vuelos retardados:",round(horaTarde,2),"horas",round(minutosTarde,2),"Minutos")
 print(" El vuelo con mayor retardo fue el:",numTarde,"con salida programada")
 print(" a las",horaProgra,":",minutoProgra,"con",demoraTarde ,"minutos de retraso")
-    
-
-
-
-
-
-
-
